5aea09f1788ab16695e48d614cd67cd0c6dc1c0b
msandalika95 <msandalika95@gmail.com>
2021-11-25 14:47:42 +0530

    Initial Commit

---
+++ WebApplication3/WebApplication3/Controllers/HomeController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            List<Customer> customers = new List<Customer>();
+            // Connect to MySQL->[s].
+            using (MySqlConnection con = new MySqlConnection("server=localhost;user=root;database=testa;port=3306;password=123456"))
+            {
+                con.Open();
+                MySqlCommand cmd = new MySqlCommand("select * from customer", con);
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read()) {
+                    // Extract your data ->[s].
+                    Customer customer = new Customer();
+                    customer.id = Convert.ToInt32(reader["id"]);
+                    customer.code = reader["code"].ToString();
+                    customer.name = reader["name"].ToString();
+
+                    customers.Add(customer);
+
+                }
+                reader.Close();
+            }
+
+
+                return View(customers);
+        }
+
+        public IActionResult Add()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+
+        //For CREATE function ->[S].
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public IActionResult Create(int id, string code, string name)
+        {
+            using (MySqlConnection con = new MySqlConnection("server=localhost;user=root;database=testa;port=3306;password=123456"))
+            {
+            con.Open();
+            var command = "insert into customer(id,code,name) values('" + id + "','" + code + "','" + name + "')";
+            MySqlCommand cmd = new MySqlCommand(command, con);
+            MySqlDataReader reader = cmd.ExecuteReader();
+            reader.Close();
+        }
+            return View();
+    }
+
+
+
+
+
+        //For UPDATE function ->[S].
+
+        public IActionResult Update()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Update(int id, string code, string name)
+        {
+            using (MySqlConnection con = new MySqlConnection("server=localhost;user=root;database=testa;port=3306;password=123456"))
+            {
+                con.Open();
+                MySqlCommand cmd = new MySqlCommand("UPDATE customer SET id = '" + id + "',code = '" + code + "', name = '" + name + "' WHERE id = " +id,con);
+                MySqlDataReader reader = cmd.ExecuteReader();
+                reader.Close();
+            }
+                return RedirectToAction("Index");
+        }
+
+        
+        
+
+
+        //For DELETE function ->[S].
+        public ActionResult Delete(int id)
+        {
+            using (MySqlConnection con = new MySqlConnection("server=localhost;user=root;database=testa;port=3306;password=123456"))
+            {
+                con.Open();
+                MySqlCommand cmd = new MySqlCommand("DELETE FROM customer WHERE id=" +id, con);
+                MySqlDataReader reader = cmd.ExecuteReader();
+                reader.Close();
+            }
+                return RedirectToAction("Index");
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
+
+
---
+++ WebApplication3/WebApplication3/Models/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication3.Models
+{
+    public class Customer
+    {
+        public int id { get; set; }
+
+        public string code { get; set; }
+
+        public string name { get; set; }
+    }
+}
